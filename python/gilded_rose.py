5ffa10f056e31b058f28053c1c7421b826462a34
Michal Charemza <michal@charemza.name>
2023-07-27 21:09:18 +0100

    simplify

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -34,10 +34,7 @@ class GildedRose(object):
                     if item.quality > 0 and item.sell_in < 1:
                         item.quality -= 1
                 elif item.quality == 1:
-                    if item.quality > 0:
-                        item.quality -= 1
-                    if item.quality > 0 and item.sell_in < 1:
-                        item.quality -= 1
+                    item.quality -= 1
                 item.sell_in -= 1
 
 class Item:
