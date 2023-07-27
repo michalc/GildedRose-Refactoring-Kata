b72a8c98946712678fc5978d3376ceadb9b6e89d
Michal Charemza <michal@charemza.name>
2023-07-27 19:55:34 +0100

    Reduce conditions

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -7,7 +7,7 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert" and item.name != "Sulfuras, Hand of Ragnaros" and item.quality > 0:
+            if item.name not in ("Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros") and item.quality > 0:
                 item.quality -= 1
             if item.name == "Aged Brie" or item.name == "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality < 50:
