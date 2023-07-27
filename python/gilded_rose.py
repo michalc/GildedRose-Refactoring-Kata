a56200cd68b92a9bf6fb11157e56b6d31be7f4ec
Michal Charemza <michal@charemza.name>
2023-07-27 20:12:58 +0100

    Separate out processing

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -29,9 +29,9 @@ class GildedRose(object):
                 pass
 
             else:
+                item.sell_in -= 1
                 if item.quality > 0:
                     item.quality -= 1
-                item.sell_in -= 1
                 if item.quality > 0 and item.sell_in < 0:
                     item.quality -= 1
 
