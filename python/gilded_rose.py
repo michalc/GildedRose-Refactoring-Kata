561a656a7e3b51db73723a1db2c5803cba7fdf5f
Michal Charemza <michal@charemza.name>
2023-07-27 20:14:57 +0100

    Separate out processing

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -15,13 +15,13 @@ class GildedRose(object):
                     item.quality += 1
 
             elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                item.sell_in -= 1
                 if item.quality < 50:
                     item.quality += 1
-                    if item.sell_in < 11 and item.quality < 50:
+                    if item.sell_in < 10 and item.quality < 50:
                         item.quality += 1
-                    if item.sell_in < 6 and item.quality < 50:
+                    if item.sell_in < 5 and item.quality < 50:
                         item.quality += 1
-                item.sell_in -= 1
                 if item.sell_in < 0:
                     item.quality = 0
 
