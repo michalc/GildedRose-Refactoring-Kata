35e6df6751548de7626239969875679e38d62c41
Michal Charemza <michal@charemza.name>
2023-07-27 20:11:54 +0100

    Separate out processing

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -8,9 +8,9 @@ class GildedRose(object):
     def update_quality(self):
         for item in self.items:
             if item.name == "Aged Brie":
+                item.sell_in -= 1
                 if item.quality < 50:
                     item.quality += 1
-                item.sell_in -= 1
                 if item.quality < 50 and item.sell_in < 0:
                     item.quality += 1
 
