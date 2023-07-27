49de4ec32129d22e7d455a76e8bfd2638f6fda5b
Michal Charemza <michal@charemza.name>
2023-07-27 20:03:26 +0100

    Reduce nesting

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -11,11 +11,10 @@ class GildedRose(object):
                 item.quality -= 1
             if (item.name == "Aged Brie" or item.name == "Backstage passes to a TAFKAL80ETC concert") and item.quality < 50:
                 item.quality += 1
-                if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                    if item.sell_in < 11 and item.quality < 50:
-                        item.quality += 1
-                    if item.sell_in < 6 and item.quality < 50:
-                        item.quality += 1
+                if item.name == "Backstage passes to a TAFKAL80ETC concert" and item.sell_in < 11 and item.quality < 50:
+                    item.quality += 1
+                if item.name == "Backstage passes to a TAFKAL80ETC concert" and item.sell_in < 6 and item.quality < 50:
+                    item.quality += 1
             if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in -= 1
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert" and item.name != "Sulfuras, Hand of Ragnaros" and item.quality > 0 and item.sell_in < 0:
