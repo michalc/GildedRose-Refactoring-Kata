a99ded088ee4d13f752f4420cfa711bf80b56550
Michal Charemza <michal@charemza.name>
2023-07-27 21:08:09 +0100

    Separate cases

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -29,10 +29,16 @@ class GildedRose(object):
                 pass
 
             else:
-                if item.quality > 0:
-                    item.quality -= 1
-                if item.quality > 0 and item.sell_in < 1:
-                    item.quality -= 1
+                if item.quality > 1:
+                    if item.quality > 0:
+                        item.quality -= 1
+                    if item.quality > 0 and item.sell_in < 1:
+                        item.quality -= 1
+                elif item.quality == 1:
+                    if item.quality > 0:
+                        item.quality -= 1
+                    if item.quality > 0 and item.sell_in < 1:
+                        item.quality -= 1
                 item.sell_in -= 1
 
 class Item:
