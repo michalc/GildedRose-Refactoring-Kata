26952aaea7e005169501d44be9974cbcd1ae8d82
Michal Charemza <michal@charemza.name>
2023-07-27 20:30:17 +0100

    Change sell in later

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -8,32 +8,32 @@ class GildedRose(object):
     def update_quality(self):
         for item in self.items:
             if item.name == "Aged Brie":
-                item.sell_in -= 1
                 if item.quality < 50:
                     item.quality += 1
-                if item.quality < 50 and item.sell_in < 0:
+                if item.quality < 50 and item.sell_in < 1:
                     item.quality += 1
+                item.sell_in -= 1
 
             elif item.name == "Backstage passes to a TAFKAL80ETC concert":
-                item.sell_in -= 1
                 if item.quality < 50:
                     item.quality += 1
-                    if item.sell_in < 10 and item.quality < 50:
+                    if item.sell_in < 11 and item.quality < 50:
                         item.quality += 1
-                    if item.sell_in < 5 and item.quality < 50:
+                    if item.sell_in < 6 and item.quality < 50:
                         item.quality += 1
-                if item.sell_in < 0:
+                if item.sell_in < 1:
                     item.quality = 0
+                item.sell_in -= 1
 
             elif item.name == "Sulfuras, Hand of Ragnaros":
                 pass
 
             else:
-                item.sell_in -= 1
                 if item.quality > 0:
                     item.quality -= 1
-                if item.quality > 0 and item.sell_in < 0:
+                if item.quality > 0 and item.sell_in < 1:
                     item.quality -= 1
+                item.sell_in -= 1
 
 class Item:
     def __init__(self, name, sell_in, quality):
